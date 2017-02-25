0d7eb180b79723f49e70e1147d2c603ccc683760
Pong <pong230@gmail.com>
2017-02-25 15:54:23 +0700

    Init WeatherDetailFragment and layout

---
+++ app/src/main/java/com/pong/myweather/fragment/WeatherDetailFragment.java
@@ -0,0 +1,27 @@
+package com.pong.myweather.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pong.myweather.R;
+
+/**
+ * Created by USER on 25/2/2560.
+ */
+
+public class WeatherDetailFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weatherdetail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
