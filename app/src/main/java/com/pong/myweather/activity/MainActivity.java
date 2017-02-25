05964839f9b482901c1842609874a07c521308be
Pong <pong230@gmail.com>
2017-02-25 14:35:21 +0700

    Prepare basic package structure

--- app/src/main/java/com/pong/myweather/activity/MainActivity.java
+++ app/src/main/java/com/pong/myweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.pong.myweather;
+package com.pong.myweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.pong.myweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
