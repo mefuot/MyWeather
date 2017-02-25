edc1b8f21c8258e3ab6d8b5e64bdf24e32bc2b1d
Pong <pong230@gmail.com>
2017-02-25 15:10:12 +0700

    Init SearchFragment and its layout

---
+++ app/src/main/java/com/pong/myweather/fragment/SearchFragment.java
@@ -0,0 +1,28 @@
+package com.pong.myweather.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pong.myweather.R;
+
+/**
+ * Created by USER on 25/2/2560.
+ */
+
+public class SearchFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
