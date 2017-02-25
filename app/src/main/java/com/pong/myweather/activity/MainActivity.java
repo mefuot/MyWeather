d9bbfe04870f6c24b7619553c9120685b2c13005
Pong <pong230@gmail.com>
2017-02-25 15:09:38 +0700

    MainActivity show SearchFragment at start app

--- app/src/main/java/com/pong/myweather/activity/MainActivity.java
+++ app/src/main/java/com/pong/myweather/activity/MainActivity.java
@@ -1,15 +1,29 @@
 package com.pong.myweather.activity;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.pong.myweather.R;
+import com.pong.myweather.fragment.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
+    SearchFragment searchFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null){
+            openSearchFragment();
+        }
+    }
+
+    private void openSearchFragment() {
+        searchFragment = new SearchFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container, searchFragment);
+        ft.commit();
     }
 }
