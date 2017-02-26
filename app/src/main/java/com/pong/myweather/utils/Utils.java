c2638019b052e8a04bdf2c7958c2920518070185
Pong <pong230@gmail.com>
2017-02-26 15:58:23 +0700

    Create Uils class and add mehod to help capitalize first charecter (Good for English but not localization friendly)

---
+++ app/src/main/java/com/pong/myweather/utils/Utils.java
@@ -0,0 +1,11 @@
+package com.pong.myweather.utils;
+
+/**
+ * Created by USER on 26/2/2560.
+ */
+
+public class Utils {
+    public static String capitalize(final String str){
+        return str.substring(0,1).toUpperCase() + str.substring(1);
+    }
+}
