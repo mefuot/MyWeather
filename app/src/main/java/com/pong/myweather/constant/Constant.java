fc569bd0e3b43022328b83bea568b278a5c2cf1c
Pong <pong230@gmail.com>
2017-02-27 16:43:09 +0700

    Add Constant class for keep internal static data

---
+++ app/src/main/java/com/pong/myweather/constant/Constant.java
@@ -0,0 +1,9 @@
+package com.pong.myweather.constant;
+
+/**
+ * Created by USER on 25/2/2560.
+ */
+
+public class Constant {
+    public static final String BASE_URL = "http://api.worldweatheronline.com/";
+}
